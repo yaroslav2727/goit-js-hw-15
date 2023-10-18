9488eccc40f4880b85ae0f757566762d7ba023c2
yaroslav2727 <90917825+yaroslav2727@users.noreply.github.com>
2023-10-18 18:02:30 +0300

    fix1

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,8 @@
 const inputEl = document.querySelector("#font-size-control");
 const textEl = document.querySelector("#text");
 
+textEl.style.fontSize = `${inputEl.value}px`;
+
 inputEl.addEventListener("input", e => {
   textEl.style.fontSize = `${e.currentTarget.value}px`;
 });
